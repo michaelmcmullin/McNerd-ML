651d876bf81f03e79a73f65522ab49db037f2db3
mrmcnerd <blueark@outlook.com>
2016-10-01 17:19:29 +0100

    Add project files.

---
+++ McNerd.MachineLearning.LinearAlgebra/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace McNerd.MachineLearning.LinearAlgebra
+{
+    public class Class1
+    {
+    }
+}
